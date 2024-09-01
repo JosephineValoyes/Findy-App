8a4b49ed0a92845d956a9ff4eda41c7847eb08ff
gercek1 <olcanchano@gmail.com>
2024-08-31 19:53:54 -0500

    ✨ [error]
    se corrigio el error de "fillRule" del icono en el navbar.

--- src/components/common/NavBar/Navbar.jsx
+++ src/components/common/NavBar/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
 
       {/* Botón Mobile Menu */}
       <button className="block md:hidden" onClick={toggleMenu}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21"><g fill="#D0D6F9" fill-rule="evenodd"><path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" /></g></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21"><g fill="#D0D6F9" fillRule="evenodd"><path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" /></g></svg>
       </button>
 
       {/* Mobile Menu */}
